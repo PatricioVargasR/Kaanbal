e8fb46acd19a5036d8ecda9960f53ea4b04f12b7
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-10-19 23:42:31 -0600

    Se añadió una navegación básica entre las páginas públicas

---
+++ components/public/nav_links_publics.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import clsx from "clsx";
+
+// Links del espacio público
+const links = [
+    { name: 'Principal', href: '/' },
+    { name: 'Precios', href: '/precios' },
+    { name: 'Acerca', href: '/acerca' },
+  ]
+
+export default function NavLinksPublic() {
+
+    // Obtenemos el nombre de la ruta, es decir https://localhost/hola -> /hola
+    const pathname = usePathname();
+
+    return(
+        <>
+            {links.map((link) => {
+                return (
+                    <Link
+                    key={link.name}
+                    href={link.href}
+                    className={clsx(
+                        'text-[#0f4c81]',
+                        {
+                        'font-bold': pathname == link.href
+                        },
+                    )}>{link.name}</Link>
+                )
+            })}
+
+        </>
+    )
+}
