48e646a44c0427e6f754e49b750f0f101562a18f
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-10-20 21:20:34 -0600

    Corrección de nombres de páginas

--- components/user/nav_links.tsx
+++ components/user/nav_links.tsx
@@ -7,15 +7,15 @@ export default function NavLinks() {
             <Link href="/" className="flex items-center space-x-2 text-[#0f4c81] font-bold">
                 <span>Kaanbal</span>
             </Link>
-            <Link href="/home" className="flex items-center space-x-2">
+            <Link href="/principal" className="flex items-center space-x-2">
                 <Home size={20} />
                 <span>Inicio</span>
             </Link>
-            <Link href="/library" className="flex items-center space-x-2">
+            <Link href="/biblioteca" className="flex items-center space-x-2">
                 <Library size={20} />
                 <span>Tu biblioteca</span>
             </Link>
-            <Link href="/notifications" className="flex items-center space-x-2">
+            <Link href="/notificaciones" className="flex items-center space-x-2">
                 <Bell size={20} />
                 <span>Notificaciones</span>
             </Link>
@@ -23,11 +23,11 @@ export default function NavLinks() {
             <   MessageSquare size={20} />
                 <span>Chat PDF</span>
             </Link>
-            <Link href="/profile" className="flex items-center space-x-2">
+            <Link href="/perfil" className="flex items-center space-x-2">
             <   User size={20} />
                 <span>Perfil</span>
             </Link>
-            <Link href="/more" className="flex items-center space-x-2">
+            <Link href="/mas" className="flex items-center space-x-2">
                 <MoreHorizontal size={20} />
                 <span>Más</span>
             </Link>
