9974c516affca01b2e8206e4708c84cb1dc1b878
alonso <1722110219@utectulancingo.edu.mx>
2024-11-19 15:28:25 -0600

    Cambios

--- components/user/nav_links.tsx
+++ components/user/nav_links.tsx
@@ -30,23 +30,23 @@ export default function NavLinks() {
             <Link href="/" className="flex items-center space-x-2 text-[#0f4c81] font-bold">
                 <span>Kaanbal</span>
             </Link>
-            <Link href="/principal" className="flex items-center space-x-2">
+            <Link href="/dashboard" className="flex items-center space-x-2">
                 <Home size={20} />
                 <span>Inicio</span>
             </Link>
-            <Link href="/biblioteca" className="flex items-center space-x-2">
+            <Link href="/dashboard/biblioteca" className="flex items-center space-x-2">
                 <Library size={20} />
                 <span>Tu biblioteca</span>
             </Link>
-            <Link href="/notificaciones" className="flex items-center space-x-2">
+            <Link href="/dashboard/notificaciones" className="flex items-center space-x-2">
                 <Bell size={20} />
                 <span>Notificaciones</span>
             </Link>
-            <Link href="/chat-pdf" className="flex items-center space-x-2">
+            <Link href="/dashboard/chat-pdf" className="flex items-center space-x-2">
             <   MessageSquare size={20} />
                 <span>Chat PDF</span>
             </Link>
-            <Link href="/perfil" className="flex items-center space-x-2">
+            <Link href="/dashboard/perfil" className="flex items-center space-x-2">
             <   User size={20} />
                 <span>Perfil</span>
             </Link>
