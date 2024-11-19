acae5cda1955225df6decacc7bcdaa113b3bf15b
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-11-19 00:02:58 -0600

    Se añadió la opción de iniciar sesión mediante las credenciales

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider  from "next-auth/providers/credentials";
+import { obtenerUnUsuario } from "@/lib/utils";
+
+const bcrypt = require('bcrypt');
 
 // Función para manejar el registro
-const handler =  NextAuth({
+const handler = NextAuth({
 
     // Define todos los proovedores
     providers: [
@@ -18,6 +22,40 @@ const handler =  NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+
+        // Proveedor de credenciales
+        CredentialsProvider({
+            name: 'Credentials',
+
+            credentials: {
+              email: { label: "Email", type: "text", placeholder: "Email" },
+              contrasena: { label: "contraseña", type: "password", placeholder: "Contraseña" }
+            },
+
+            async authorize(credentials, req) {
+
+                // Verificar que los datos estén presentes
+                if (!credentials?.email || !credentials.contrasena) {
+                    throw new Error('Email y contraseña son requeridos')
+                }
+
+                // Buscar el usuario por el email
+                const usuarioEncontrado = await obtenerUnUsuario(credentials.email);
+
+                // Verificar que exista
+                if (!usuarioEncontrado) throw new Error('No se eoncotró el usaurio');
+
+                // Verificar la contraseña
+                const esContrasenaValida = await bcrypt.compare(credentials.contrasena, usuarioEncontrado.contrasena)
+
+                // Si la contraseña es incorrecta, lanza un error
+                if (!esContrasenaValida) throw new Error('Contraseña incorrecta')
+
+                // Si el usuario y la contraseña son válidos
+                return { id: usuarioEncontrado.id_usuario.toString(), email: usuarioEncontrado.email }
+
+            }
         })
     ],
 
