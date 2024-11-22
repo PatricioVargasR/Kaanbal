b1f6c8440677739ab154e6e72b050234d91ab1f2
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-11-21 20:07:06 -0600

    Se eliminó el archivo sesion.ts

--- lib/sesion.ts
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-
-// Función para obtener la sesión
-export async function obtenerSesion() {
-
-    // Utilizando getServerSession obtiene la sesión iniciada
-    const sesion = await getServerSession()
-    return sesion;
-}
