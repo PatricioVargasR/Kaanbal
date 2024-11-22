ea359ba3ccb44e1304d2380fefb994f36811af1d
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-11-21 20:25:01 -0600

    Actualización de footer

--- components/user/footer.tsx
+++ components/user/footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 export default function Footer() {
     return(
       <footer className="bg-white p-4 text-center text-sm">
-        <Link href="/privacy" className="mx-2">Privacy Policy</Link>
-        <Link href="/terms" className="mx-2">Terms</Link>
-        <Link href="/about" className="mx-2">About</Link>
+        <Link href="/privacy" className="mx-2">Política de privacidad</Link>
+        <Link href="/terms" className="mx-2">Términos</Link>
+        <Link href="/about" className="mx-2">Acerca</Link>
       </footer>
     )
 }
