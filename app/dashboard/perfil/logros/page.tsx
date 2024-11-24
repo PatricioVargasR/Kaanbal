c96cc0bb3846b085206dd8eb67b5f1d0bc065c85
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-11-24 13:27:33 -0600

    Corrección de los iconos de logros

--- app/dashboard/perfil/logros/page.tsx
+++ app/dashboard/perfil/logros/page.tsx
@@ -1,13 +1,12 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
-import { obtenerIdUsuario, obtenerSesion, obtenerUnUsuario, obtenreProgresoLogros } from "@/lib/utils"
+import { obtenerSesion, obtenerUnUsuario, obtenreProgresoLogros } from "@/lib/utils"
 import { Trophy, Star, Clock, Book, Target, Zap } from 'lucide-react'
 
 export default async function AchievementsPage() {
 
   // Obtiene la sesión del usuario
   const sesion = await obtenerSesion()
-
 
   // Obtiene el id_usuario con base a su email
   const usuario = await obtenerUnUsuario(sesion?.user?.email?.toString())
@@ -22,6 +21,17 @@ export default async function AchievementsPage() {
     ))
   ))
 
+  // Mapeo de los íconos según su nombre
+  const iconos: Record<string, JSX.Element> = {
+    "Zap": <Zap className="h-6 w-6" />,
+    "Trophy": <Trophy className="h-6 w-6" />,
+    "Star": <Star className="h-6 w-6" />,
+    "Clock": <Clock className="h-6 w-6" />,
+    "Book": <Book className="h-6 w-6" />,
+    "Target": <Target className="h-6 w-6" />,
+  };
+
+
   return (
       <div className="space-y-6">
         <h1 className="text-3xl font-bold text-[#0f4c81]">Logros</h1>
@@ -31,7 +41,7 @@ export default async function AchievementsPage() {
             <Card key={indice}>
               <CardHeader>
                 <CardTitle className="flex items-center space-x-2">
-                  {/* <logro.icon className="h-6 w-6" /> */}
+                  {iconos[logro.icono_logro] || <Zap className="h-6 w-6"/>}
                   <span>{logro.nombre_logro}</span>
                 </CardTitle>
               </CardHeader>
