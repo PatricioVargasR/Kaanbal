373f7a469237bb4bdeee05c96ff9d473b4fce4c9
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-12-11 23:57:40 -0600

    Cambios de politica

--- components/user/footer.tsx
+++ components/user/footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 export default function Footer() {
     return(
       <footer className="bg-white p-4 text-center text-sm">
-        <Link href="/privacy" className="mx-2">Política de privacidad</Link>
-        <Link href="/terms" className="mx-2">Términos</Link>
-        <Link href="/about" className="mx-2">Acerca</Link>
+        <Link href="/dashboard/politica" className="mx-2">Política de privacidad</Link>
+        <Link href="/dashboard/terminos" className="mx-2">Términos</Link>
+        <Link href="/acerca" target="_blank" className="mx-2">Acerca</Link>
       </footer>
     )
 }
